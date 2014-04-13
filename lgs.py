129691bb2670418aa6341ea4130f434762800e39
Andrey Belyak <beliak@mail.ru>
2014-04-13 05:05:07 +0400

    Все ф-ции команд получают произвольнй набор агрументов, команда pl приведена в соответствие со специикацией протокола

--- lgs.py
+++ lgs.py
@@ -44,21 +44,21 @@ def command(keyword=None):
 
 
 @command(keyword=commands.CLEAR_BUFFER)
-def clear_buffer():
+def clear_buffer(*args, **kwargs):
     GD.collected_lines = []
     msg = 'collected lines has been dropped.'
     return create_message(CODE_OK, msg)
 
 
 @command(keyword=commands.PUT_LINE)
-def put_line():
-    GD.collected_lines.append(io_stream.read())
+def put_line(line_without_command, *args, **kwargs):
+    GD.collected_lines.append(line_without_command)
     msg = 'line has been collected (%d at the moment).' % len(GD.collected_lines)
     return create_message(CODE_OK, msg)
 
 
 @command(keyword=commands.CALC)
-def calc():
+def calc(*args, **kwargs):
     data = GD.GLOBAL_DATA['current']
     words_count = 0
     for l in GD.collected_lines:
@@ -75,25 +75,23 @@ def calc():
 
 
 @command(keyword=commands.PRINT_STATS)
-def print_stats():
-    lines = []
+def print_stats(*args, **kwargs):
+    lines = ['STATISTICS']
     current_stats = GD.GLOBAL_DATA['current']
+    """:type: dict"""
     for i, v, in current_stats.items():
-        lines.append('%s\t%d' % (i, v))
+        lines.append('%s %d' % (i, v))
     return create_message(200, '\n'.join(lines))
 
 
 @command(commands.EXIT)
-def close_session():
-    """
-    exit: closes current session
-    """
-    return 'OK. Good bye!'
+def close_session(*args, **kwargs):
+    return create_message(200, 'OK. Good bye!')
 
 
-COMMANDS += [
-    ('ver', lambda: (str(VERSION)))
-]
+@command(commands.VERSION)
+def get_version(*args, **kwargs):
+    return create_message(200, str(VERSION))
 
 if __name__ == '__main__':
     io_stream.write(HELLO_MSG)
@@ -106,6 +104,6 @@ if __name__ == '__main__':
             bin_cmd = cmd.encode()
             cmd_len = len(bin_cmd)
             if line[:cmd_len] == bin_cmd:
-                message = callback()
+                message = callback(line_without_command=line[cmd_len + 1:])
                 io_stream.write(message)
                 break
