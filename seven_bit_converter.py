9c1ae9d781c2b2ef0c44bec72940002890f0523b
Andrey Belyak <beliak@mail.ru>
2014-05-17 16:13:55 +0400

    написан метод обратной конвертации, тест пройден.

--- seven_bit_converter.py
+++ seven_bit_converter.py
@@ -12,21 +12,36 @@ def iterate_as_bits(in_bytes, with_ix=False):
             yield (in_byte >> weight) & 1
 
 
+def significant_bits(in_bits):
+    """
+    генератор, выдающий только значимые биты кодированной последовательности:
+    >>> list(significant_bits([0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1]))
+    [1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1]
+    """
+    for ix, in_bit in enumerate(in_bits):
+        if ix % 8 == 0:
+            continue
+        yield in_bit
+
+
 class SevenBitConverter():
     """
     Класс, преобразующий нормальный поток сервера в кодировку 7BM и обратно.n
     """
+
+    def __init__(self):
+        self.bit_weight = lambda n: 2**n  # вес бита
+
     def encode(self, normal_message):
         """
         :param bytes normal_message: сообщение которое требуется перекодировать в последовательность 7BM
         """
         result = []
         out_byte = 0
-        bit_weight = lambda n: 2**n
 
         for ix, in_bit in enumerate(iterate_as_bits(normal_message)):
             bit_position = 6 - (ix % 7)
-            out_byte += in_bit * bit_weight(bit_position)
+            out_byte += in_bit * self.bit_weight(bit_position)
             if bit_position == 0:
                 result.append(out_byte)
                 out_byte = 0
@@ -37,4 +52,14 @@ class SevenBitConverter():
         """
         :param bytes seven_bit_message: закодированное сообщение, которое требуется декодировать.
         """
-        return seven_bit_message
+        result = []
+        out_byte = 0
+
+        for ix, in_bit in enumerate(significant_bits(iterate_as_bits(seven_bit_message))):
+            bit_position = 7 - (ix % 8)
+            out_byte += in_bit * self.bit_weight(bit_position)
+            if bit_position == 0:
+                result.append(out_byte)
+                out_byte = 0
+
+        return bytes(result)
--- tests/test_seven_bit_converter.py
+++ tests/test_seven_bit_converter.py
@@ -7,22 +7,24 @@ class SevenBitConverterTest(TestCase):
     def setUp(self):
         super().setUp()
         self.converter = SevenBitConverter()
-
+        self.normal_bytes = bytes([200, 200, 200, 200, 200, 200, 200])
+        self.decoded_7_bits = bytes([100, 50, 25, 12, 70, 35, 17, 72])
 
     def test_encode_divisible_case(self):
         """
         Проверка перекодирования сообщения, кратный случай:
         текстовое сообщение 7 байт по 8 бит --> 8 байт по 7 бит
         """
-        normal_bytes = bytes([200, 200, 200, 200, 200, 200, 200])
-        expected_7bit = bytes([100, 50, 25, 12, 70, 35, 17, 72])
 
-        result_7bit = self.converter.encode(normal_bytes)
+        result_7bit = self.converter.encode(self.normal_bytes)
 
-        self.assertEquals(expected_7bit, result_7bit)
+        self.assertEquals(self.decoded_7_bits, result_7bit)
 
-    # def test_encode_non_divisible_case(self):
-    #     self.fail()
-    #
-    # def test_decode(self):
-    #     self.fail()
+    def test_decode(self):
+        """
+        проверка декодирования 7бит в 8бит, кратный случай.
+        сообщение 8 байт с 7 значащими битами в 7 "нормальных" байт
+        """
+        result_8bit = self.converter.decode(self.decoded_7_bits)
+
+        self.assertEquals(self.normal_bytes, result_8bit)
