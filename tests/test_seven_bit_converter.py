d8e3458889bbe19b1d8506792604c54b9a0dbc1f
Andrey Belyak <beliak@mail.ru>
2014-05-18 15:16:55 +0400

    переименованы переменные

--- tests/test_seven_bit_converter.py
+++ tests/test_seven_bit_converter.py
@@ -8,7 +8,7 @@ class SevenBitConverterTest(TestCase):
         super().setUp()
         self.converter = SevenBitConverter()
         self.normal_bytes = bytes([200, 200, 200, 200, 200, 200, 200])
-        self.decoded_7_bits = bytes([100, 50, 25, 12, 70, 35, 17, 72])
+        self.encoded_7_bits = bytes([100, 50, 25, 12, 70, 35, 17, 72])
 
     def test_encode_divisible_case(self):
         """
@@ -18,7 +18,7 @@ class SevenBitConverterTest(TestCase):
 
         result_7bit = self.converter.encode(self.normal_bytes)
 
-        self.assertEquals(self.decoded_7_bits, result_7bit)
+        self.assertEquals(self.encoded_7_bits, result_7bit)
 
     def test_encode_non_divisible_case(self):
         """
@@ -34,6 +34,6 @@ class SevenBitConverterTest(TestCase):
         проверка декодирования 7бит в 8бит, кратный случай.
         сообщение 8 байт с 7 значащими битами в 7 "нормальных" байт
         """
-        result_8bit = self.converter.decode(self.decoded_7_bits)
+        result_8bit = self.converter.decode(self.encoded_7_bits)
 
         self.assertEquals(self.normal_bytes, result_8bit)
